5d13e109d702bbf2c001b62d52fac7371eab5a27
Saksham Paliwal <sakshampaliwal11@gmail.com>
2023-08-01 23:43:34 +0530

    Majority Element more than N/3

--- Arrays/MajorityElement_NByThree.java
+++ Arrays/MajorityElement_NByThree.java
@@ -42,6 +42,53 @@ public class MajorityElement_NByThree {
         }
         return list;
     }
+
+    // Optimal Approach(Moore's Voting):
+    public static List<Integer> fun3(int[] arr) {
+        int n=arr.length;
+        int c1=0;
+        int c2=0;
+        int el1=0;
+        int el2=0;
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            if(c1==0 && arr[i]!=el2){
+                c1++;
+                el1=arr[i];
+            }
+            else if(c2==0 && arr[i]!=el1){
+                c2++;
+                el2=arr[i];
+            }
+            else if(arr[i]==el1){
+                c1++;
+            }
+            else if(arr[i]==el2){
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        
+        int count1=0,count2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==el1){
+                count1++;
+            }
+            else if(arr[i]==el2){
+                count2++;
+            }
+        }
+        if(count1>n/3){
+            list.add(el1);
+        }
+        if(count2>n/3){
+            list.add(el2);
+        }
+        return list;
+    }
     public static void main(String[] args) {
         int nums[] = {3,2,3};
         System.out.println(fun1(nums));
