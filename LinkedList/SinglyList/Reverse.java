d8420825da0ccd700eef87b1d251a9bb85262099
Saksham Paliwal <sakshampaliwal11@gmail.com>
2023-08-05 23:58:19 +0530

    Reverse Iteratively

---
+++ LinkedList/SinglyList/Reverse.java
@@ -0,0 +1,30 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Reverse {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        while(head!=null){
+            ListNode next=head.next;
+            head.next=prev;
+            prev=head;
+            head=next;
+        }
+        head=prev;
+        return head;
+    }
+}
